91ec3cbc02cdecd3a42be32c5bc9048a2f7c3bb6
nshiff <nshiff@users.noreply.github.com>
2023-12-02 09:46:15 -0500

    test 2 working

--- src/components/PuzzleSolver.tsx
+++ src/components/PuzzleSolver.tsx
@@ -8,39 +8,66 @@ export function PuzzleSolver(){
 
     function getMultilineStringAsArray(str:string){
         const arr = str.split('\n');
-        return arr;
-            // .map(line => line.trim())
-            // .filter(line => line.length > 0);
+        return arr
+            .map(line => line.trim())
+            .filter(line => line.length > 0);
     }
 
     function solvePuzzle(lines: string[]): number {
         console.log(lines)
+        
+        const MAX_BALLS = {
+            RED: 12,
+            GREEN: 13,
+            BLUE: 14
+        };
+        
         let sum = 0;
-        for(const line of lines){
-            let iFirstNum = null;
-            let iLastNum = 0;
-            for(let i=0; i<line.length; i++){
-                if(isDigit(line[i])){
-                    if(iFirstNum === null){
-                        iFirstNum = i;
-                        // console.log('iFirstNum:', iFirstNum)
+        let i = 1;
+        for (const lineFull of lines){
+            const iGame = lineFull.indexOf(':')
+            const line = lineFull.slice(iGame + 1)
+            console.log(line)
+
+            const rgb ={
+                red: 0,
+                green: 0,
+                blue: 0,
+            }
+
+            const rounds = line.split(';');
+            for(const round of rounds){
+                console.log(round)
+                const balls = round.split(',');
+                for(const ball of balls){
+                    console.log(ball)
+                    const numberColor = ball.trim().split(' ')
+                    console.log(numberColor)
+                    if(numberColor[1] === 'red'){
+                        rgb.red += Number.parseInt(numberColor[0])
                     }
-                    iLastNum = i;
-                    // console.log('iLastNum:', iLastNum)
+                    else if(numberColor[1] === 'green'){
+                        rgb.green += Number.parseInt(numberColor[0])
+                    }
+                    else if(numberColor[1] === 'blue'){
+                        rgb.blue += Number.parseInt(numberColor[0])
+                    }
                 }
             }
-            const calibrationValueStr = `${line[iFirstNum ?? 0]}${line[iLastNum]}`;
-            const calibrationValue = Number.parseInt(calibrationValueStr);
-            sum += calibrationValue;
-            // console.log('calibrationValueStr:', calibrationValueStr)
-            // console.log('calibrationValue:', calibrationValue)
-            // console.log('sum:', sum)
+            console.log(rgb)
+            if(rgb.red <= MAX_BALLS.RED
+                && rgb.green <= MAX_BALLS.GREEN 
+                && rgb.blue <= MAX_BALLS.BLUE){
+                sum += i;
+            }
+            i += 1;
         }
+
         return sum;
     }
 
 
-    const answer = solvePuzzle(getMultilineStringAsArray(PuzzleConstants.input01));
+    const answer = solvePuzzle(getMultilineStringAsArray(PuzzleConstants.testinput02));
 
     console.log('---');
     console.log(answer);
