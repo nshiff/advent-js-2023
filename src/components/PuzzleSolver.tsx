98128fadeff0244d13b74521c063f6b96760bae1
nshiff <nshiff@users.noreply.github.com>
2023-12-03 08:44:36 -0500

    day 03

--- src/components/PuzzleSolver.tsx
+++ src/components/PuzzleSolver.tsx
@@ -1,99 +1,33 @@
-import { PuzzleConstants } from "./PuzzleConstants";
-
 export function PuzzleSolver(){
-
-    function isDigit(str:string) {
-        return /^\d+$/.test(str);
-    }
-
-    function getMultilineStringAsArray(str:string){
-        const arr = str.split('\n');
-        return arr
-            .map(line => line.trim())
-            .filter(line => line.length > 0);
-    }
-
-    function solvePuzzle(lines: string[]): number {
-        console.log(lines)
-        
-        const MAX_BALLS = {
-            RED: 12,
-            GREEN: 13,
-            BLUE: 14
-        };
-        
-        let sum = 0;
-        let i = 1;
-        for (const lineFull of lines){
-            const iGame = lineFull.indexOf(':')
-            const line = lineFull.slice(iGame + 1)
-            console.log('---')
-            console.log(i)
-            console.log(line)
-
-            const rgb ={
-                red: 0,
-                green: 0,
-                blue: 0,
-            }
-
-            const rounds = line.split(';');
-            for(const round of rounds){
-                console.log(round)
-                const balls = round.split(',');
-                for(const ball of balls){
-                    console.log(ball)
-                    const numberColor = ball.trim().split(' ')
-                    console.log(numberColor)
-                    if(numberColor[1] === 'red'){
-                        rgb.red += Number.parseInt(numberColor[0])
-                    }
-                    else if(numberColor[1] === 'green'){
-                        rgb.green += Number.parseInt(numberColor[0])
-                    }
-                    else if(numberColor[1] === 'blue'){
-                        rgb.blue += Number.parseInt(numberColor[0])
-                    }
-                }
-            }
-            console.log(rgb)
-            if(rgb.red <= MAX_BALLS.RED
-                && rgb.green <= MAX_BALLS.GREEN 
-                && rgb.blue <= MAX_BALLS.BLUE){
-                sum += i;
-            }
-            i += 1;
-        }
-
-        return sum;
-    }
-
-    function solvePuzzleAdventJS(gifts:any[], materials:string) {
-        const giftsToReturn:any[] = [];
+    function solvePuzzleAdventJS(gifts:number[]) {
+        const iRepeated = []
       
-        for(const gift of gifts){
-            let canBuild = true;    
-            for(let i=0; i<gift.length; i++){
-                console.log(gift[i]) // todo compare to material
-                if(!materials.includes(gift[i])){
-                    canBuild = false;
-                    break;
-                }
-            }
-            if(canBuild){
-                giftsToReturn.push(gift);
-            }
+        for(let i=0; i<gifts.length; i++){
+        //   debugger;
+          const subArray = gifts.slice(i + 1)
+          const match = subArray.indexOf(gifts[i])
+          console.log('match:',match)
+          if(match === -1){
+            continue;
+          }
+          iRepeated.push(match + i + 1);
+          console.log('iRepeated:',iRepeated)
         }
       
-        return giftsToReturn
+        if(iRepeated.length === 0){
+          return -1;
+        }
+        const iAnswer = Math.min(...iRepeated)
+        console.log('iAnswer:',iAnswer)
+        console.log('gifts[iAnswer]:',gifts[iAnswer])
+        return gifts[iAnswer]
       }
+      
 
     // const answer = solvePuzzle(getMultilineStringAsArray(PuzzleConstants.input02));
-    const answer = solvePuzzleAdventJS(['tren', 'oso', 'pelota'], 'tronesa');
-
-
+    const answer = solvePuzzleAdventJS([2, 1, 3, 5, 3, 2]);
 
     console.log('---');
-    console.log(answer);
+    // console.log(answer);
     return <h3>puzzle solver</h3>;
 }
