c837aed816d76df06f44d9ccbb6a0e6d07bb4a27
nshiff <nshiff@users.noreply.github.com>
2023-12-21 10:36:44 -0500

    day 21, 'balanced' binary strings

--- src/components/PuzzleSolver.tsx
+++ src/components/PuzzleSolver.tsx
@@ -1,32 +1,58 @@
 export function PuzzleSolver(){
+    function solvePuzzle(message:number[]) {
+      let iStart = null;
+      let iEnd = null;
+    
+      function isSequenceBalanced(i:number,j:number){
+        let subArray = message.slice(i, j+1);
+        console.log(subArray)
 
-    function isDigit(char:string){
-      return !Number.isNaN(parseInt(char));
-    }
-
-    function solveAdventJsPuzzle(arr:number[]) {
-      let countPositive = 0;
-      let countZero = 0;
-      let countNegative = 0;
-      
-      arr.forEach(num => {
+        let count0s = 0;
+        let count1s = 0;
+        subArray.forEach(num => {
           if(num === 0){
-              countZero += 1;
-          }
-          else if(num > 0){
-              countPositive += 1;
+            count0s++;
           }
           else{
-              countNegative += 1;
+            count1s++;
           }
-      })
-      
-      console.log((countPositive / arr.length).toFixed(6))
-      console.log((countNegative / arr.length).toFixed(6))
-      console.log((countZero / arr.length).toFixed(6))
+        })
+        return count0s === count1s;
+      }
+
+      console.log('message:', message)
+      for(let i=0; i<message.length - 1; i++){
+        for(let j=i+1; j<message.length; j++){
+          let isBalanced = isSequenceBalanced(i, j);
+          console.log('isBalanced:',isBalanced)
+          if(isBalanced){
+            console.log('iStart:', iStart);
+            console.log('iEnd:', iEnd);
+            console.log('i:', i);
+            console.log('j:', j);
+            if(iStart === null || iEnd === null){
+              iStart = i;
+              iEnd = j;
+              console.log('keep1');
+              continue
+            }
+            else if((j - i) > (iEnd - iStart)){
+              iStart = i;
+              iEnd = j;
+              console.log('keep2');
+              continue
+            }
+            console.log('skip');
+          }
+
+        }
+
+      }
+
+      return iStart !== null ? [iStart, iEnd] : [];
     }
 
-    const answer = solveAdventJsPuzzle([-4, 3, -9, 0, 4, 1]);
+    const answer = solvePuzzle([1, 0, 1, 0]);
 
     console.log(answer);
     console.log('---');
